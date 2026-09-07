ace6703769434d5b48f61c353219a819a9c3f829
Brent Strydom <wistudi.team@gmail.com>
2026-09-07 18:45:51 +0700

    Wire live event registration submit bridge on preview branch

--- functions/resources/events/building-a-communicative-esl-lesson-with-flow/_middleware.js
+++ functions/resources/events/building-a-communicative-esl-lesson-with-flow/_middleware.js
@@ -3,6 +3,9 @@ export async function onRequest(context){
   const type=response.headers.get('content-type')||'';
   if(!type.includes('text/html')) return response;
   return new HTMLRewriter()
-    .on('body',{element(el){el.append('<script src="/assets/js/event-registration-component.js" defer></script>',{html:true});}})
+    .on('body',{element(el){
+      el.append('<script src="/assets/js/event-registration-component.js" defer></script>',{html:true});
+      el.append('<script src="/assets/js/event-registration-live-bridge.js" defer></script>',{html:true});
+    }})
     .transform(response);
 }
