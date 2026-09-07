03d5d3645495fe4a35484d5e21ca31f9b9a9c52b
Brent Strydom <wistudi.team@gmail.com>
2026-09-07 17:52:36 +0700

    Inject event registration component on workshop page

---
+++ functions/resources/events/building-a-communicative-esl-lesson-with-flow/_middleware.js
@@ -0,0 +1,8 @@
+export async function onRequest(context){
+  const response=await context.next();
+  const type=response.headers.get('content-type')||'';
+  if(!type.includes('text/html')) return response;
+  return new HTMLRewriter()
+    .on('body',{element(el){el.append('<script src="/assets/js/event-registration-component.js" defer></script>',{html:true});}})
+    .transform(response);
+}
